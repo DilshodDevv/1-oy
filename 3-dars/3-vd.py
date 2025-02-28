e810040c18ffa6aa20fc87c04710b5abd2761009
Dilshod <odilshod679.com>
2025-02-28 19:42:42 +0500

    3-dars

---
+++ 3-dars/3-vd.py
@@ -0,0 +1,42 @@
+"""
+2.27.2025
+3- dars
+Maavzu : O'ZGARUVCHILAR
+"""
+#Muallif : Dilshod Ochilov
+"""O'zgaruvchi —kompyuter xotirasida ma'lum bir qiymatni saqlash uchun ajratilgan joy.
+ Soddaroq qilib tushuntirsak, o'zgaruvchini quti, quti ichidagi narsani esa qiymat deb tasavvur qilish mumkin.
+   Pythonda qiymatlar son, matn, ro'yxat va hokazo ko'rinishida bo'lishi mumkin."""
+ism = "Abdulloh"
+yosh = 25
+print(ism)
+print(yosh)
+# bu yirda biz ism va yosh degan uzgaruvchi ishlatayapmiz
+"""O'zgaruvchilarga nom berishda quyidagi qoidalarga amal qiling:
+
+O'zgaruvchi nomi harf yoki pastki chiziq (_) bilan boshlanishi kerak
+
+O'zgaruvchi nomi raqam bilan boshlanishi mumkin emas
+
+O'zgaruvchi nomida faqatgina lotin alifbosi harflari (A-z), raqamlar (0-9) va pastki chiziq (_) qatnashishi mumkin
+
+O'zgaruvchi nomida bo'shliq (пробел) bo'lishi mumkin emas
+
+O'zgaruvchi nomida katta-kichik harflar turlicha talqin qilinadi (ism, ISM, va Ism uchta turli o'zgaruvchi)"""
+ism = "Abdulloh"
+print(ism)
+ism="Muhammad"
+print(ism)
+print(ism)
+print(ism)
+# o'zgaruvchining nomini kichik harflar bilan yozing
+# O'zgaruvchi nomida  2 va undan ortiq so'z qatnashsa ularni orasini (_) bilan ajrating (ism_sharif="Ochilov Dilshod")
+#O'zgaruvchiga  tushunarli nom bering ( y=20 emas yosh=20, d="Korea" emas davlat="Korea" va hakazo
+#shuningdek o'zgaruvchilarga Pythonda ishlatiladigan funksiyalar va maxsus kalit so'zlar (keywords) nomini bermang
+# "Keywordslar"
+# folse ; class ; from ; or ; None ; continue ; global ; pass;
+# true ; def ; if ; raise ; and ; del ; import ; return ;
+# as ; elif ; in ; try ; assert ; else ; is ; while ; async ;
+# except ; lambda ; with ; await ; finally ; nonlocal ; yield ; break ; for ; not ;
+
+
