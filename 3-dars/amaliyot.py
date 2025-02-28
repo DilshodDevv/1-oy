47a28abab84f968cdaaf9b7466d338beb23955b1
Dilshod <odilshod679.com>
2025-02-28 22:54:50 +0500

    o'zgartirdim

--- 3-dars/amaliyot.py
+++ 3-dars/amaliyot.py
@@ -1,2 +1,18 @@
-salom = "Hello World"
+"""
+2,28,2025
+Dastyurlash aasoslari
+3-dars : O'ZGARUVCHILAR
+Muallif:Dilshod Ochilov
+
+"""
+salom = " Hello World "
 print(salom)
+xabar = "salom shahzod qalaysiz ishlAR yaxshimi"
+print(xabar)
+xabar = " yaxshi raxmad"
+#class deb uzgaruvchi yaratib bulmaydi chunki class paytonning keywordisi sanaladi
+print(xabar)
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi*radius**2
+print("Radusi" , radius , "ga teng aylananing yuzi=", aylana_yuzi)
