17c9a80b48c38ccf74dbe56d9b83c5dd3ac1d26c
Dilshod <odilshod679.com>
2025-03-08 02:04:21 +0500

    Yangi fayl

---
+++ 5-dars/5-vd.py
@@ -0,0 +1,73 @@
+"""
+3,7,2025
+5-dars
+MAVZU:Sonlar
+Muallif:Dilshod Ochilov
+"""
+#a = 20 #musbat yoki manfiy bulishi mumkin
+#b = -30
+#c=a+b
+#v= a-b
+#print(c)
+#print(v)
+# Kvadratni yuzini hisoblaymiz
+#kvdrt_tmni = 20 # Kvadtarni to moni 20 ga teng
+#kvdrt_yuzi = kvdrt_tmni**2 # Kvadrat yuzini hisoblaymiz
+#print(kvdrt_yuzi)
+#Pythonda o('nlik sonlar floating point numbers yoki qisqa qilib floats deyiladi. "Floating point numbers" atamasini'
+# ozbek tiliga "suzuvchi nuqtali sonlar" deb tarjima qilish mumkin. Ingliz tilida o('nlik sonlarni yozishda vergul '
+#'(,) emas nuqta (.) belgisi ishlatiladi, va bu nuqta sonning katta kichikligiga qarab joyi o')zgargani uchun "floating"
+#(suzuvchi) deyiladi.
+#pi = 3.14156 #o'nlik son (float)
+#radusi = 10 # butun son iniger
+#diametr = 2*radusi
+#print("Aylananing uzunligi ", pi*diametr, "ga teng.")
+#avval aytganimizdek ikki butun sonni bulish(/) natijasida o'nlik son hosl buladi(natija butun ham buladi
+#a = -20
+#b= 40
+#c = b/a
+#print(c) # natija o'nlik son buladi
+#shuningdek butun va unlik sonlar o'rtasida har qanday arfmetik amallarning natijasi ham unlik son buladdi
+#a= 2
+#b=3.0
+#c = b/a
+#print(c)
+#quyidagi arfmetik amallarni natijasi o'nlik son hosil qiladi
+#print(a+b)
+#print(a*b)
+#print(a**b)
+#print(2*(a+b))
+#Uzun sonlarni kiritishda, qulaylik uchun, raqamlarni pastki chiziq (_) yordamida guruhlash mumkin.
+#Python - son tarkibidagi pastki chiziqlarni (_) inobatga olmasdan, uzun sonligicha qabul qiladi.
+#aholi_soni = 7_594_000_000# o'zimizga qulay bulishi uchun shunday yozdik
+#print("yer kurrasida", aholi_soni, "ga yaqin odam yashaydi")
+#Aksar dasturlash tillarida konstant qiymatlar tushunchasi bor. Konstantlar o'zgarmas bo'ladi (misol uchun
+#π
+#π ning qiymati konstant, o'zgarmas qiymat). Pythonda konstant tushunchasi yo'q, shuning uchun dasturchilar bunday
+#o'zgaruvchilarning nomini katta harflar bilan yozadilar (ogohlantirish sifatida). Bu albatta qat'iy qonun emas,
+#lekin kelajakda o'zgaruvchilar orasida konstant qiymatlarni ajratish uchun yaxshi usul.
+#PI = 3.14159
+#radius = 21.2
+#Birdaniga bir nechta o'zgaruvchiga qiymat berish uchun o'zgaruvchilar va ularga mos qiymatlar vergul (,) bilan ajratila
+#x,y,z = 10,-7.25, -30
+#Yuqoridagi kod x ga 10, y ga -7.25, va z ga -30 qiymatini yuklaydi.
+#Keling quyidagi misolni ko'raylik, maqsadimiz ism va yosh degan ikki o'zgaruvchini yangi xabar degan o('zgaruvchiga '
+#yuklab, "Jobir 16 yoshda" degan matnni konsolga chiqarish:)
+#ism = 'JOBIR'
+#yosh = 36
+#xabar= ism + ' ' + str(yosh) + ' yoshdA'
+#print(xabar)
+#str()— int yoki float turidagi sonlarni matnga o'zgartiradi.
+
+#int()— matn yoki float ko'rinishidagi qiymatlarni butun songa o'zgartiradi. Bunda matn butun son ko'rinishida bo'lishi kerak.
+
+#float()— matn yoki int ko'rinishidagi qiymatlarni o'nlik songa o'zgartiradi.
+#ism = 'Jobir'
+#yosh = 36
+#print(type(ism))#ism degan uzgaruvchining qiymatini konsolga chiqaramiz
+#print(type(yosh))#ismyosh degan o'zgaruvchining turini konsolga chiqariladi
+#t_yil = int(input("Tug'ilga yilingizni kiriting"))
+#yosh = 2025 - (t_yil)
+#print('Siz ' + 2007
+#str(yosh) + " yoshda ekansiz")
+#input() funksiyssi har qaday kiritilgan qiymatni matn (string)kurinishida qabul qiladi
---
+++ 5-dars/amaliyot.py
@@ -0,0 +1,17 @@
+"""
+3,7,2025
+5- dars
+MAVZU : SONLAR
+MUALLIF :Ochilov Dilshod
+"""
+#son = int (input("istallgan sonni kiriting: \n>>>"))
+#print(son, 'siz kiritgan sonni kivadrati' , son**2 , 'siz kiritgan sonni kubi' , son**3 , 'shunga teng')
+#yosh = int (input("tug'ilgan yilingizni kiriting :\n>>>"))
+#x = 2025-yosh
+#print(x)
+a = float(input('istalgan son kiriting:\n>>>'))
+b = int(input('istalgan son kiriting:\n>>>'))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
