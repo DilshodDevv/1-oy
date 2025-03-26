bf335d72ab83a0dc03b0bc6ac18a187afed464cf
Dilshod <odilshod679.com>
2025-03-27 00:42:15 +0500

    Yangi fayl

---
+++ 9-dars/9-vd.py
@@ -0,0 +1,46 @@
+"""
+3,21,2025
+9-dars
+MAVZU: IF ELSE
+MUALLIF: DILSHOD OCHILOV
+"""
+#if OPERATORI
+#ifsuzi ingliz tilidan "agar degan manoni anglatadi >>> Dastur tilida shartlarni bajarish uchun ishlatiladi
+# avtolar=['audi', 'bmw', 'volvo', 'kia', 'hyundai']
+# for avto in avtolar : #avtolar ichidagi har bitta avtolar uchun
+#      if avto == 'bmw' : #agar avto teng bulsa 'bmw' ga teng bulsa
+#          print(avto.upper()) # avto nomini hamma harrflarini katta harf bilan yoz
+#      else: # aks holda ...
+#          print(avto.title()) # avto nomini faqat birinchi harifini katta harf bilan yoz
+# == => tengmi degani
+# != => teng emasmi
+#TRUE/FALSE
+#yuqoridagi shaartni bajarish uchun == operatoridan
+#bu operatorni oddiy tilga tarjima qilsak"tengmi?"  degan manoni beradi
+# Agar shartning ikkala tarafidagi qiymati teng bulsa ifoda TRUE qiymat qaytaradi ("true" suzi ingliz tilidan "haqiqiy
+ #yoki "tug'ri " degan manoni anglatadi)
+#Aksinncha , qiymatloar tenglikni qanoatlantirmasa, ifoda folse qiymat qaytaradi
+# (folse suzi ingliz tilidan "yolg'on degan manoni anglatadi
+# ism = "Ali"
+# print(ism == "Ali") # true qiymat qaytaradi
+# print(ism == "Vali") # folse qiymat qaytaradi
+#demak , if/else bog'lamasida, if ning badani True bo'lganda,else ning badani esa ifoda Folse bo'lganda bajariladi
+#MATNLARNI SOLISHTIRISH
+# Xo'sh, turli ko'rinishda yozilgan matnlarni qanday qilib solishtiramiz? Juda oddiy. Matnlarni solishtirishdan avval
+# .lower() metodi yordamida kichik harflar ko'rinishiga keltirib olamiz:
+# != =>teng emas degan shart
+#
+# Kichik: a<b
+#
+# Kichik yoki teng: a<=b
+#
+# Katta: a>b
+#
+# Katta yoki teng: a>=b
+#
+#
+# javob = float(input("12x6 nechiga teng?>>>"))
+# if javob!=72:
+#     print("Javob xato!")
+#
+
---
+++ 9-dars/amaliyot.py
@@ -0,0 +1,50 @@
+"""
+3,21,2025
+9-dars
+MAVZU:IF ELSE
+MUALLIF:DILSHOD OCHILOV
+"""
+# cars = ['toyota', 'mazda' , 'huyundai', 'gm', 'kia']
+# for moshin in  cars:
+#     if moshin == "gm":
+#         print(moshin.upper())
+#     else:
+#         print(moshin.title())
+# xonadoshlar = ['dilshod' , 'burhoniddin' , 'shahzod' , 'diyor']
+# for aka in xonadoshlar:
+#     if aka == "diyor":
+#         print(aka.title() , 'aka')
+#     else:
+#         print(aka.title())
+# cars=['toyota' ,'mazda' ,'huyundai' ,'gm' ,'kia']
+# for moshin in cars:
+#     if moshin !="gm":
+#         print(moshin.title())
+#     else:
+#         print(moshin.upper())
+# membir = input("loginingizni kiriting>>>\n")
+# if membir.lower()=="admin":
+#     print("Xush kelibsiz Admin .Foydalanuvchilar ro'yxatini kurasizmi?")
+# else:
+#     print(f"xush kelibsiz , {membir} ")
+# x = float(input("birinchi sonni kiriting\n>>>"))
+# y = float(input("ikkinchi sonni kiriting\n>>>"))
+# if x==y :
+#     print("sonlar teng ", x ," = ",y)
+# else:
+#     print(f"sonlar teng emas")
+# son = int(input("istalgan son kiriting\n>>>"))
+# if son < 0:
+#     print("manfiy")
+# elif son>0:
+#     print("musbat")
+# else:
+#     print("0")
+# son = int(input("istalgan son kiriting\n>>>"))
+# if son>0 :
+#     print(f"sonning kivadrati {son**2}")
+# elif son<0:
+#     print("musbat son kiriting")
+# else:
+#     print(0)
+
