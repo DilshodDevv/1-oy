9d7bb572541c013892eba59a72275bb4392e0b94
Dilshod <odilshod679.com>
2025-09-22 00:53:10 +0500

    yangilanish

--- 8-dars/amaliyot.py
+++ 8-dars/amaliyot.py
@@ -21,3 +21,22 @@ MUALLIF: DILSHOD OCHILOV
 # for k in range(suhbat):
 #     ismlar.append(input(f"{k+1}-suhbat qilgan odamingiz kim edi: "))
 #     print(ismlar)
+ismlar = ["Ali ","Vali ","hasan ","husan ","shohruh "]
+for ism in ismlar:
+    print(ism)
+for taklif in ismlar :
+    print("salom "+ taklif)
+    print(f"kod {len(ismlar)} marta takrorlandi")
+sonlar=list(range(11,100,2))
+print(sonlar)
+for kubi in sonlar :
+    print("kubi \n >>>" +str( kubi**3))
+# kino=[]
+# for kinolar in range(5) :
+#     kino.append(input(f"{kinolar + 1} istalgan sevimli kinongizni kiriting"))
+# print(kino)
+suhbatlashganlar=[]
+soni = int(input("bugun nechta odam bilan suhbatlashdiz\n>>>"))
+for n in range(soni) :
+    suhbatlashganlar.append(input(f"{n+1} - si"))
+print(suhbatlashganlar)
